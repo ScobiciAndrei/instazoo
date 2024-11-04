fe88baac2a8c0269b46585326e5d0c489308c28b
Tutorial <scobiciandrei95@gmail.com>
2024-11-05 00:39:41 +0100

    create user entity and role

---
+++ src/main/java/instazoo/entity/User.java
@@ -0,0 +1,29 @@
+package instazoo.entity;
+
+import instazoo.entity.enums.ERole;
+import jakarta.persistence.PrePersist;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class User {
+    private Long id;
+    private String name;
+    private String userName;
+    private String lastName;
+    private String email;
+    private String bio;
+    private String password;
+
+    private Set<ERole> role = new HashSet<>();
+    private List<Post> posts = new ArrayList<>();
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
+}

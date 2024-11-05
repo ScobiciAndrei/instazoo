1120dd170db14ef01ef62b541036fd9d2fa638f2
Tutorial <scobiciandrei95@gmail.com>
2024-11-05 09:42:53 +0100

    add annotation for user

--- src/main/java/instazoo/entity/User.java
+++ src/main/java/instazoo/entity/User.java
@@ -1,7 +1,9 @@
 package instazoo.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import instazoo.entity.enums.ERole;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -9,17 +11,31 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(unique = true, updatable = false)
     private String userName;
+    @Column(nullable = false)
     private String lastName;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
 
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",joinColumns = @JoinColumn(name = "user_id"))
     private Set<ERole> role = new HashSet<>();
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,mappedBy = "user",orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+    @JsonFormat(pattern = "yyyy-nn-dd HH:mm:ss")
     private LocalDateTime createdDate;
 
     @PrePersist

17a7005fb88c9d7f5a915069630e5a08a486a74e
Tutorial <scobiciandrei95@gmail.com>
2024-11-11 11:09:32 +0100

    add annotation to class

--- src/main/java/instazoo/repository/PostRepository.java
+++ src/main/java/instazoo/repository/PostRepository.java
@@ -3,12 +3,14 @@ package instazoo.repository;
 import instazoo.entity.Post;
 import instazoo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
-    List<Post> findAllByOrderByCreatedDateDesc(User user);
+   /* List<Post> findAllByOrderByCreatedDateDesc(User user);*/
     List<Post> findAllByOrderByCreatedDateDesc();
     Optional<Post> findPostByIdAndUser(Long id,User user);
 }

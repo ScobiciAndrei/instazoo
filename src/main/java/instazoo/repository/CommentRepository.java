80eb1a6284aa8c5020d9b156cd5ecddb3e4c470f
Tutorial <scobiciandrei95@gmail.com>
2024-11-11 11:09:04 +0100

    add annotation @Repository to class

--- src/main/java/instazoo/repository/CommentRepository.java
+++ src/main/java/instazoo/repository/CommentRepository.java
@@ -3,9 +3,11 @@ package instazoo.repository;
 import instazoo.entity.Comment;
 import instazoo.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     List<Comment> findAllByPost(Post post);
     Comment findByIdAndUserId(Long commentId, Long userId);
